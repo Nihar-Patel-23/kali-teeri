aa7005cd1c35b7c2e91f0cbb6a718a895676b8b7
Nihar-Patel-23 <niharp123@gmail.com>
2025-08-16 02:34:10 -0400

    codes

--- firebase.js
+++ firebase.js
@@ -1,31 +1,28 @@
-// Import the functions you need
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set, get, onValue } from "firebase/database";
-import { getAuth, signInAnonymously } from "firebase/auth";
+// firebase.js (CDN modules + anonymous auth)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import {
+  getDatabase, ref, onValue, set, update, get, child, serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js";
+import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 
-// Your Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDNKd-6bqLTJ6xm_6MWdMOlc7SeUTxoitQ",
   authDomain: "kaali-teeri-56002.firebaseapp.com",
   databaseURL: "https://kaali-teeri-56002-default-rtdb.firebaseio.com",
   projectId: "kaali-teeri-56002",
-  storageBucket: "kaali-teeri-56002.firebasestorage.app",  // 👈 This might be wrong, should be "firebasestorage.googleapis.com"
+  storageBucket: "kaali-teeri-56002.appspot.com", // <- typical value; use what Firebase shows
   messagingSenderId: "730464476936",
   appId: "1:730464476936:web:c04f447e9a3d20908c96bd",
   measurementId: "G-8YLVXSMW1F"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
 
-// Database
-const db = getDatabase(app);
+// sign in anonymously so rules with `auth != null` work
+export const auth = getAuth(app);
+signInAnonymously(auth).catch(e => console.error("Anon auth failed:", e));
 
-// Authentication
-const auth = getAuth(app);
-signInAnonymously(auth)
-  .then(() => console.log("Signed in anonymously"))
-  .catch((error) => console.error("Auth error:", error));
+// re-export DB helpers because app.js imports them from here
+export { ref, onValue, set, update, get, child, serverTimestamp };
 
-// Export what the rest of your app needs
-export { db, ref, set, get, onValue, auth };
